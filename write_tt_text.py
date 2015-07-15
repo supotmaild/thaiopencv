f6e10f3064488314d2bb079c18f1b04e530c595b
Supot Sawangpiriyakij <therithwarich@gmail.com>
2015-07-15 14:18:03 +0700

    Update write_tt_text.py

--- write_tt_text.py
+++ write_tt_text.py
@@ -1,36 +1,7 @@
 # image is whole capture image webcam from openCV
 
 def write_tt_text(image,x,y,text):
-    win_text=''
-    chk1=0
-    chk2=0
-    for k in text:
-        if chk2>0:
-            if chk2==1:
-                kk=ord(k)+3456
-            else:
-                kk=ord(k)+3520
-            chk1=0
-            chk2=0
-        else:    
-            if chk1==1 and chk2==0:
-                if ord(k)==184:
-                    chk2=1
-                elif ord(k)==185:
-                    chk2=2
-                continue
-            if ord(k)==224:
-                chk1=1
-                continue
-            else:
-                kk=ord(k)
-        vv=0
-        if kk>=3584:
-            kk=kk-3424
-            kk=kk-160
-            win_text=win_text+unichr(0x0E00 + kk)
-        else:
-            win_text=win_text+unichr(0x0000 + kk)
+    win_text=text.decode('UTF-8')
     win_text=win_text+unichr(0x0020)+unichr(0x0020)
     Font1 = ImageFont.truetype("angsau_0.ttf", 14)
     image_pil = Image.new("RGB",(150,26),(0,0,0))
